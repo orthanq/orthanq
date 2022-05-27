2d5d9037cea13be358d805505c37fafd8ed28bec
huzuner <hamdiye.uzuner@uni-due.de>
2022-05-27 13:19:01 +0000

    fmt tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -35,10 +35,16 @@ fn check_haplotype_fractions_5050() {
     let mut output = std::path::PathBuf::new();
     output.push("test_output_5050.csv");
     let _ = orthanq::calling::haplotypes::CallerBuilder::default()
-        .hdf5_reader(hdf5::File::open("tests/Sample_HLA00601-0.5_HLA21879-0.5_DQA1_abundance.h5").unwrap())
+        .hdf5_reader(
+            hdf5::File::open("tests/Sample_HLA00601-0.5_HLA21879-0.5_DQA1_abundance.h5").unwrap(),
+        )
         .haplotype_variants(bcf::Reader::from_path("tests/hla-allele-variants_v4.vcf.gz").unwrap())
-        .haplotype_calls(bcf::Reader::from_path("tests/Sample_HLA00601-0.5_HLA21879-0.5.bcf").unwrap())
-        .observations(bcf::Reader::from_path("tests/Sample_HLA00601-0.5_HLA21879-0.5_obs.bcf").unwrap())
+        .haplotype_calls(
+            bcf::Reader::from_path("tests/Sample_HLA00601-0.5_HLA21879-0.5.bcf").unwrap(),
+        )
+        .observations(
+            bcf::Reader::from_path("tests/Sample_HLA00601-0.5_HLA21879-0.5_obs.bcf").unwrap(),
+        )
         .min_norm_counts(0.01)
         .max_haplotypes(2)
         .use_evidence("both".to_string())
@@ -50,7 +56,10 @@ fn check_haplotype_fractions_5050() {
     //check if the haplotypes are correct
     let mut rdr = csv::Reader::from_path("test_output_5050.csv").unwrap();
     let headers = rdr.headers().unwrap();
-    assert_eq!(vec![&headers[2], &headers[3]], vec!["HLA:HLA00601", "HLA:HLA21879"]); //both are from locus DQA1.
+    assert_eq!(
+        vec![&headers[2], &headers[3]],
+        vec!["HLA:HLA00601", "HLA:HLA21879"]
+    ); //both are from locus DQA1.
 
     //check if the fractions are 0.5 each
     let mut iter = rdr.records();
