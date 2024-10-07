e9d7b1a32eb0c122f40e88da029853a8b30e513a
huzuner <hamdiyeuzuner@gmail.com>
2024-10-07 12:12:25 +0000

    fix: completely remove if-else block

--- src/calling/haplotypes/haplotypes.rs
+++ src/calling/haplotypes/haplotypes.rs
@@ -53,15 +53,8 @@ impl AlleleFreqDist {
             let y_0_prob = y_0.exp();
             let y_1_prob = y_1.exp();
             let density = NotNan::new(y_0_prob).unwrap()
-                + (*vaf - *x_0) * (y_1_prob - y_0_prob) / (*x_1 - *x_0); //calculation of density for given vaf by linear interpolation
-                                                                         //handle the case where density has 0 probability, which results in panicking with "FloatIsNan" error.
-                                                                         //to avoid another floatisnan error, we have to be sure to return only probs that are certainly greater than 0.0 (0.000...)
-                                                                         //otherwise it should return zero probability, but in log space. (not 1 with ln_one() because density=0.0 means the probability is impossible.)
-            if density > NotNan::new(0.0).unwrap() {
-                Some(LogProb::from(Prob(NotNan::into_inner(density))))
-            } else {
-                Some(LogProb::ln_zero())
-            }
+                + (*vaf - *x_0) * (y_1_prob - y_0_prob) / (*x_1 - *x_0);
+            Some(LogProb::from(Prob(NotNan::into_inner(density))))
         }
     }
 }
