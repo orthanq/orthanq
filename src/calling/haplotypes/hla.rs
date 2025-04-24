d4d9cbb51d6db645954e5867532acdb5aa7a6976
huzuner <hamdiyeuzuner@gmail.com>
2025-04-24 12:44:15 +0000

    fmt

--- src/calling/haplotypes/hla.rs
+++ src/calling/haplotypes/hla.rs
@@ -166,26 +166,29 @@ impl Caller {
             //this is because we only use 3-field resolution of the haplotypes. This means, some haplotypes can
             // directly match, e.g. 24:436 while some e.g. A*24:03:01 have longer names in the xml.
             for haplotype in &all_haplotypes {
-                    let mut found_match = false;
-                
-                    for (allele, g_group) in &allele_to_g_groups {
-                        if allele == &haplotype.to_string() || allele.starts_with(&haplotype.to_string()) {
-                            if g_group == "None" { //in case the allele has "None" in the g group field in the xml.
-                                final_haplotypes_converted.push(haplotype.clone());
-                            } else {
-                                final_haplotypes_converted.push(Haplotype(g_group.to_string()));
-                            }
-                            found_match = true;
-                            break; 
+                let mut found_match = false;
+
+                for (allele, g_group) in &allele_to_g_groups {
+                    if allele == &haplotype.to_string()
+                        || allele.starts_with(&haplotype.to_string())
+                    {
+                        if g_group == "None" {
+                            //in case the allele has "None" in the g group field in the xml.
+                            final_haplotypes_converted.push(haplotype.clone());
+                        } else {
+                            final_haplotypes_converted.push(Haplotype(g_group.to_string()));
                         }
-                    }
-                
-                    if !found_match {
-                        //in case the allele does not have a corresponding g group field in the xml.
-                        final_haplotypes_converted.push(Haplotype(haplotype.to_string()));
+                        found_match = true;
+                        break;
                     }
                 }
-                
+
+                if !found_match {
+                    //in case the allele does not have a corresponding g group field in the xml.
+                    final_haplotypes_converted.push(Haplotype(haplotype.to_string()));
+                }
+            }
+
             dbg!(&final_haplotypes_converted);
             haplotypes::write_results(
                 &converted_name,
